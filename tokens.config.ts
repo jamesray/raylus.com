004d5bce654a9e63f7fece66bab5e67d3f3f6027
James Ray <james.a.ray@jpl.nasa.gov>
2023-08-14 02:12:08 -0700

    updates tokens

--- tokens.config.ts
+++ tokens.config.ts
@@ -4,9 +4,44 @@ export default defineTheme({
   alpine: {
     body: {
       backgroundColor: {
-        initial: '#FFFFF',
+        initial: '#fcf6e8',
         dark: '#0c0c0d'
+      },
+
+      color: {
+        initial: '#2f2c2e',
+        dark: '#fcf6e8'
       }
+    },
+
+    readableLine: '64ch'
+  },
+
+  prose: {
+    p: {
+      fontSize: '20px',
+      margin: '1.8rem auto'
+    },
+
+    h5: {
+      margin: '3rem 0 10',
+      fontSize: '24px',
+      fontWeight: '700'
+    },
+
+    hr: {
+      margin: '14px 0',
+      style: 'dotted',
+      width: '6px',
+
+      color: {
+        initial: '#a1a1aa',
+        dark: 'var(--color-gray-600)'
+      }
+    },
+
+    table: {
+      fontSize: '16px'
     }
   }
 })
