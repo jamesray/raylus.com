50ba57d955ebfc81b0eab53c11b00856046d0f1d
James Ray <james.a.ray@jpl.nasa.gov>
2023-08-14 01:19:49 -0700

    Update tokens.config.ts

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,43 +1,5 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
-  text: {
-    base: {
-      fontSize: '1.2rem'
-    },
-
-    lg: {
-      fontSize: '1.15rem'
-    }
-  },
-
-  elements: {
-    container: {
-      maxWidth: '66rem'
-    }
-  },
-
-  prose: {
-    li: {
-      margin: '0.6rem 0'
-    },
-
-    table: {
-      margin: '20px 0'
-    }
-  },
-
-  alpine: {
-    body: {
-      backgroundColor: {
-        initial: '#fcf6e8',
-        dark: '#2f2c2e'
-      },
-
-      color: {
-        initial: '#2f2c2e',
-        dark: '#fcf6e8'
-      }
-    }
-  }
+  // Design Tokens that override Alpine defaults
 })
